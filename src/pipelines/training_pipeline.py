a54a858742fb9669448b14c5f61f69073bd5b325
Maaz Sayyed <115174288+MaazSayyed05@users.noreply.github.com>
2023-09-12 00:58:58 +0530

    third commit

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -1,7 +1,11 @@
 import os,sys
+
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformaiton import DataTransformation
 from src.components.model_trainer import ModelTraining
+from src.components.RGB_preporcessing import ImagePreprocessing
+from src.components.RGB_model_training import RGB_ModelTraining
+
 from src.logger import logging
 from src.exception import CustomException
 import pandas as pd
