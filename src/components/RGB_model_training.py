b8b67df58f646b908f56fe9f20f90cbd8a32e25a
Maaz Sayyed <115174288+MaazSayyed05@users.noreply.github.com>
2023-09-25 22:43:59 +0530

    Update RGB_model_training.py

--- src/components/RGB_model_training.py
+++ src/components/RGB_model_training.py
@@ -8,9 +8,9 @@ import tensorflow as tf
 import keras
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Dropout
-from keras.applications import  VGG16
-from keras.applications import ResNet50
-from keras.applications import InceptionV3
+from keras.applications.VGG16 import  VGG16
+from keras.applications.ResNet50 import ResNet50
+from keras.applications.InceptionV3 import IncepitonV3
 
 import os,sys
 from src.exception import CustomException
@@ -21,40 +21,37 @@ from src.utils import model_compile_fit
 
 
 class ModelTrainingConfig:
-    model_path = os.path.join('/config/workspace/CardioVascular_Disease_Prediction/artifacts', 'RGB_model.h5')
+    model_path = os.path.join('/config/workspace/CardioVascular_Disease_Prediction/artifacts', 'image_model.h5')
 
 
-class RGB_ModelTraining:
+class Image_ModelTraining:
 
     def __init__(self):
         self.model_train_config = ModelTrainingConfig()
 
     
-    def rgb_model_training(self, train_data_set, validation_data_set, test_data_set):
-
+    def model_training(self, train_data_set, validation_data_set, test_data_set):
         try: 
-            
-            logging.info("Initiate RGB Image Model Training.")
-            IMAGE_SIZE = [224,224]
-            CHANNELS = [3]  # RGB 
-            
+            logging.info("Initiate Image Model Training.")
+
+            IMAGE_SIZE = train_data_set.image_shape
+
 
             # Model Dict.
             # Define model fit, compile, define base_model  in utils
 
             base_models = {
-                'VGG16'           : VGG16(input_shape=IMAGE_SIZE+CHANNELS, weights='imagenet', include_top=False),
-                'RESNET50'        : ResNet50(weights='imagenet', include_top=False, input_shape=IMAGE_SIZE+CHANNELS),
-                'InceptionV3'     : InceptionV3(input_shape=IMAGE_SIZE+CHANNELS, weights='imagenet', include_top=False),
+                # 'VGG16'           : VGG16(input_shape=IMAGE_SIZE, weights='imagenet', include_top=False),
+                'RESNET101'        : ResNet101(weights='imagenet', include_top=False, input_shape=IMAGE_SIZE ),
+                'InceptionV3'     : InceptionV3(input_shape=IMAGE_SIZE, weights='imagenet', include_top=False),
+                'RESNET50'        : ResNet50(weights='imagenet', include_top=False, input_shape=IMAGE_SIZE ),
             }
 
 
-            accuracy_score_list, loss_score_list, model_fit_list = model_compile_fit(base_models,train_data_set,validation_data_set,test_data_set)
-
-            logging.info("Defining of Model, Fully Connected Layers, Conpilation and Fitting of Models Successful.")
+            accuracy_score_list, loss_score_list, model_fit_list, model_history_list = model_compile_fit(base_models,train_data_set,validation_data_set,test_data_set)
 
             best_score_index = accuracy_score_list.index(max(accuracy_score_list))
-            
+
             best_model_name = list(base_models.keys())[best_score_index]
 
             best_model = model_fit_list[best_score_index]
@@ -63,7 +60,12 @@ class RGB_ModelTraining:
 
             best_model_accuracy = accuracy_score_list[best_score_index]
 
-            logging.info(f"Best RGB Model: {best_model_name}\nModel Accuracy: {best_model_accuracy}\t    Model Loss: {best_model_loss}")
+            best_model_history = model_history_list[best_score_index]
+
+
+            logging.info(f"History of Model Training Process: \n{pd.DataFrame(best_model_history.history)}")
+
+            logging.info(f"Best Model: {best_model_name}\nModel Accuracy: {best_model_accuracy}\t    Model Loss: {best_model_loss}")
             
             # Save Model
             best_model.save(self.model_train_config.model_path)
@@ -72,7 +74,7 @@ class RGB_ModelTraining:
 
 
         except Exception as e:
-            logging.info("Error occured in RGB Image Model Training Process.")
+            logging.info("Error occured in Image Model Training Process.")
             raise CustomException(e, sys)
 
 
